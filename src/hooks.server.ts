df800836b449cf5c62197c0b939266e622eee181
Lujing <lujingcen@gmail.com>
2025-02-27 00:22:36 -0800

    warm up

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,11 @@
+import { Random } from '$lib/random';
 import { computeMetadata } from '$lib/server/metadata';
+import { invokeModel } from '$lib/server/model';
 import type { ServerInit } from '@sveltejs/kit';
 
 /**
- * Initializes the server. Called once on startup.
+ * Initializes the server. Computes conversation metadata and warms up the model.
  */
 export const init: ServerInit = async () => {
-	await computeMetadata();
+	await Promise.all([computeMetadata(), invokeModel(new Random(), '')]);
 };
