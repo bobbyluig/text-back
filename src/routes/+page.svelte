6533c34f5b0f5279b7ed980ed05c6fb75a23e32a
Lujing <lujingcen@gmail.com>
2025-03-10 00:55:35 -0700

    intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import { QuestionBank, type Question } from '$lib/question';
 	import { preloadChat, renderQuestion, revealChat, type RenderedChat } from '$lib/render';
 	import { fade } from 'svelte/transition';
+	import IconSend from '~icons/material-symbols/send';
 
 	const questionBank = new QuestionBank({ initialSeed: '70', maxCacheSize: 1 });
 
@@ -10,6 +11,7 @@
 	let question: Question | undefined = $state();
 	let score: number = $state(0);
 	let streak: number = $state(0);
+	let welcome: boolean = $state(true);
 
 	function submit(answer: string) {
 		if (chat === undefined || question === undefined) {
@@ -47,25 +49,39 @@
 		chat = c;
 		question = q;
 	}
-	// nextQuestion();
+	nextQuestion();
 </script>
 
 <main
 	class="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-fuchsia-900 to-pink-900"
 >
-	<!-- <h1 class="text-6xl text-pink-300">Text Back</h1> -->
-
-	{#key chat}
-		{#if chat}
-			<div class="absolute">
-				<ChatContainer {chat} {score} {streak} {submit} />
-			</div>
-		{:else}
-			<div
-				class="animate-spin h-10 w-10 border-4 border-green-100 rounded-full border-t-transparent"
-				in:fade|global={{ delay: 400 }}
-				out:fade|global
-			></div>
-		{/if}
-	{/key}
+	{#if welcome}
+		<div class="absolute">
+			<button
+				class="text-6xl text-white flex items-center cursor-pointer gap-2 px-4 py-2 rounded-2xl transition-colors hover:bg-gray-200 hover:text-gray-800 hover:shadow-xl"
+				onclick={() => {
+					welcome = false;
+				}}
+			>
+				<span>Text Back</span>
+				<span><IconSend /></span>
+			</button>
+		</div>
+	{:else}
+		{#key chat}
+			{#if chat}
+				<div class="absolute">
+					<ChatContainer {chat} {score} {streak} {submit} />
+				</div>
+			{:else}
+				<div class="absolute">
+					<div
+						class="animate-spin h-10 w-10 border-4 border-white rounded-full border-t-transparent"
+						in:fade|global={{ delay: 400 }}
+						out:fade|global
+					></div>
+				</div>
+			{/if}
+		{/key}
+	{/if}
 </main>
