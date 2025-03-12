2234b7def7e3f219c3d846d0affd6eca14bcee58
Lujing <lujingcen@gmail.com>
2025-03-11 23:27:02 -0700

    better style

--- src/lib/components/ContentAudio.svelte
+++ src/lib/components/ContentAudio.svelte
@@ -29,19 +29,14 @@
 </script>
 
 <div class="relative">
-	<button class="flex items-center px-4 py-2 gap-2 rounded-2xl cursor-pointer" onclick={togglePlay}>
-		<div>
-			{#if paused}
-				<IconPlay class="w-10 h-10" />
-			{:else}
-				<IconPause class="w-10 h-10" />
-			{/if}
-		</div>
-		<div class="flex items-center">
-			{#each Array(3) as _}
-				<IconWaveform class="w-10 h-10" />
-			{/each}
-		</div>
+	<button class="flex items-center px-4 py-2 gap-1 rounded-2xl cursor-pointer" onclick={togglePlay}>
+		<IconWaveform class="w-10 h-10" />
+		{#if paused}
+			<IconPlay class="w-10 h-10" />
+		{:else}
+			<IconPause class="w-10 h-10" />
+		{/if}
+		<IconWaveform class="w-10 h-10" />
 	</button>
 
 	<audio bind:this={audio} {src} onended={onEnded} class="hidden"></audio>
