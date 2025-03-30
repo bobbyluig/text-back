48d74ab914567f305b542d921fdad5077877dd82
Lujing <lujingcen@gmail.com>
2025-03-30 15:29:20 -0700

    better initialization

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,12 @@
+import { dev } from '$app/environment';
+import { Random } from '$lib/random';
 import { computeMetadata } from '$lib/server/metadata';
+import { invokeModel } from '$lib/server/model';
 import type { ServerInit } from '@sveltejs/kit';
 
 /**
  * Initializes the server by computing metadata.
  */
 export const init: ServerInit = async () => {
-	await computeMetadata();
+	await Promise.all([computeMetadata(), ...(!dev ? [invokeModel(new Random(), '')] : [])]);
 };
